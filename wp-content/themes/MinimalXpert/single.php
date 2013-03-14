a6c94af2b06c3987c6ddb0cad03f48767a4996e4
Temba Mazingi <tembamazingi@gmail.com>
2013-03-14 15:58:20 +0200

    Theme modifications.

--- wp-content/themes/MinimalXpert/single.php
+++ wp-content/themes/MinimalXpert/single.php
@@ -43,9 +43,11 @@ $options = get_option( 'mx_theme_settings' );
   if ( is_singular( 'players' ) )
   {
     $display_bio = $bio = '';
+    $player_age          = get_post_meta(get_the_ID(), 'player_age', 'true');
     $home_town          = get_post_meta(get_the_ID(), 'home_town', 'true');
     $date_joined        = get_post_meta(get_the_ID(), 'date_joined', 'true');
     $career_highlights  = get_post_meta(get_the_ID(), 'career_highlights', 'true');
+    $bio .= strlen($player_age) > 0 ? '<section><h1>Age:</h1><p>' . $player_age . '</p></section>': '';
     $bio .= strlen($home_town) > 0 ? '<section><h1>Hometown:</h1><p>' . $home_town . '</p></section>': '';
     $bio .= strlen($date_joined) > 0 ? '<section><h1>Date Joined:</h1><time datetime="' . $date_joined .'">' . date('d F Y', strtotime($date_joined)) . '</time></section>': '';
     $bio .= strlen($career_highlights) > 0 ? '<section><h1>Career Highlights:</h1><p>' . $career_highlights . '</p></section>': '';
